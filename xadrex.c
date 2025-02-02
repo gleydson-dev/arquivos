ea74ed21a54168acb3a314eb9f09d82139538d70
gleydson-dev <gleydsonj@yahoo.com.br>
2025-02-02 08:39:15 -0300

    Update xadrex.c
    jogo de cartas

--- xadrex.c
+++ xadrex.c
@@ -1,15 +1,32 @@
+Desafio Super Trunfo, By Gleydson Alves
 #include <stdio.h>
 
 int main (){
-    printf("Desafio Xadrez");
+    printf("Desafio Xadrez: \n");
+   
+    char Estado;
+    char Cidade;
+    int Populacao;
+    float Area;
+    float Pib;
+    int Ponto;
 
+    printf("Digite seu Estado \n");
+    scanf("%c", &Estado);
+    printf("Digite sua Cidade \n");
+    scanf("%c", &Cidade);
+    printf("Digite sua Populacao \n");
+    scanf("%d", &Populacao);
+    printf("Area \n");
+    scanf("%f", &Area);
+    printf("Pib \n");
+    scanf("%f", &Pib);
+    printf("Ponto Tur \n");
+    scanf("%d", &Ponto);
+    
 
+    
+ return 0;
 
-
-
-
-
-
-
-
+    
 }
